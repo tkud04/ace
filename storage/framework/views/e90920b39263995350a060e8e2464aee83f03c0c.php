c409c4695bfaef6cf1b6c777c8c2f65987c9bb8a
ANonymous <postmaster.richmama@gmail.com>
2020-06-15 23:03:17 +0100

    fixing new issues

--- storage/framework/views/e90920b39263995350a060e8e2464aee83f03c0c.php
+++ storage/framework/views/e90920b39263995350a060e8e2464aee83f03c0c.php
@@ -137,7 +137,7 @@
 			   wu: "<?php echo e($wu); ?>",
 			   ccu: "<?php echo e($ccu); ?>",
 			   pd: ppd,
-			   description: "<?php echo e($description); ?>",
+			   description: `<?php echo e($description); ?>`,
 			   inStock: "<?php echo e($in_stock); ?>",
 			   amount: "<?php echo e($amount); ?>",
 			   imggs: imggs,
@@ -203,22 +203,22 @@
                         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 product-item-container effect-wrap effect-animate">
                           <div class="product-main">
                             <div class="product-view">
-                              <figure class="double-img"><a href="<?php echo e($uu); ?>"><img class="btm-img" src="<?php echo e($imggs[0]); ?>" width="215" height="240"  alt=""/> <img class="top-img" src="<?php echo e($imggs[1]); ?>" width="215" height="240"  alt=""/></a></figure>
+                              <figure class="double-img"><a href="<?php echo e($uu); ?>"><img class="btm-img" src="<?php echo e($imggs[1]); ?>" width="215" height="240"  alt=""/> <img class="top-img" src="<?php echo e($imggs[0]); ?>" width="215" height="240"  alt=""/></a></figure>
                             </div>
                             <div class="product-btns  effect-content-inner">
-                              <p class="effect-icon"> <a href="javascript:void(0)"  onclick="addToCart({sku:'<?php echo e($sku); ?>'})" class="hint-top" data-hint="Add To Cart"><span class="cart ion-bag"></span></a></p>
-                              <p class="effect-icon"> <a href="javascript:void(0)"  onclick="addToWishlist({sku:'<?php echo e($sku); ?>'})" class="hint-top" data-hint="Wishlist"><span class="fav ion-ios-star"></span></a></p>
-                              <p class="effect-icon"> <a href="javascript:void(0)"  onclick="addToCompare({sku:'<?php echo e($sku); ?>'})" class="hint-top" data-hint="Compare"> <span class="compare ion-android-funnel"></span> </a></p>
-                              <p class="effect-icon">
-		   <a data-toggle="modal" data-target="#quick-view-box" onclick="populateQV('<?php echo e($sku); ?>','<?php echo e($description); ?>','<?php echo e($amount); ?>','<?php echo e($amount + 1000); ?>','<?php echo e(ucwords($in_stock)); ?>','<?php echo e($imggs[0]); ?>')" class="hint-top" data-hint="Quick View"><span class="ion-ios-eye view"></span> </a>
-							  </p>
+		   <p class="effect-icon"> <a href="javascript:void(0)" onclick="addToCart({sku:'<?php echo e($sku); ?>',qty:'1'})" class="hint-top" data-hint="Add To Cart"><span class="cart ion-bag"></span></a></p>
+                              <p class="effect-icon"> <a href="javascript:void(0)" onclick="addToWishlist({sku:'<?php echo e($sku); ?>'})" class="hint-top" data-hint="Wishlist"><span class="fav ion-ios-star"></span></a></p>
+                              <p class="effect-icon"> <a href="javascript:void(0)" onclick="addToCompare({sku:'<?php echo e($sku); ?>'})" class="hint-top" data-hint="Compare"> <span class="compare ion-android-funnel"></span> </a></p>
+                               <p class="effect-icon">
+		   <a data-toggle="modal" data-target="#quick-view-box" onclick="populateQV({sku:'<?php echo e($sku); ?>',description:`<?php echo e($description); ?>`,amount:'<?php echo e($amount); ?>',oldAmount:'<?php echo e($amount + 1000); ?>',inStock:'<?php echo e(ucwords($in_stock)); ?>',imgg:'<?php echo e($imggs[0]); ?>'})" class="hint-top" data-hint="Quick View"><span class="ion-ios-eye view"></span> </a>
+		  				  </p>
                             </div>
                           </div>
                           <div class="product-info">
                             <h3 class="product-name"><a href="<?php echo e($uu); ?>"><?php echo e($sku); ?></a></h3>
                             <p class="group inner list-group-item-text"><?php echo e($description); ?></p>
-                            <div class="product-price"><span class="real-price text-info"><strong>&#8358;<?php echo e(number_format($amount,2)); ?></strong></span> <span class="old-price">&#8358;<?php echo e(number_format($amount + 1000,2)); ?></span> </div>
-                            <div class="product-evaluate text-info"> <i class="ion-android-star"></i><i class="ion-android-star"></i><i class="ion-android-star"></i><i class="ion-android-star"></i><i class="ion-android-star-half"></i> </div>
+                            <div class="product-price"><span class="real-price text-info"><strong>&#8358;<?php echo e(number_format($amount,2)); ?></strong></span> <!--  <span class="old-price">&#8358;<?php echo e(number_format($amount + 1000,2)); ?></span> --></div> 
+                          <div class="product-evaluate text-info"> <i class="ion-android-star"></i><i class="ion-android-star"></i><i class="ion-android-star"></i><i class="ion-android-star"></i><i class="ion-android-star-half"></i> </div>
                           </div>
                         </div>
                         <!--end of product item container-->
