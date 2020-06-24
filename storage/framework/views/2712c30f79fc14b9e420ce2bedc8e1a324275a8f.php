93cb643aaddc4f902173ec3b542efac4c19f9303
ANonymous <postmaster.richmama@gmail.com>
2020-06-24 11:24:41 +0100

    debug bank payment

--- storage/framework/views/2712c30f79fc14b9e420ce2bedc8e1a324275a8f.php
+++ storage/framework/views/2712c30f79fc14b9e420ce2bedc8e1a324275a8f.php
@@ -21,7 +21,7 @@ foreach($items as $i)
 ?>
 
 <a href="<?php echo e($pu); ?>" target="_blank">
-  <img class="img img-fluid" src="<?php echo e($img); ?>" alt="<?php echo e($sku); ?>" height="50" width="50" style="margin-bottom: 5px;"/>
+  <img style="vertical-align: middle;border:0;line-height: 20px;" src="<?php echo e($img); ?>" alt="<?php echo e($sku); ?>" height="80" width="80" style="margin-bottom: 5px;"/>
 	  <?php echo e($sku); ?>
 
 </a> (x<?php echo e($qty); ?>)<br>
