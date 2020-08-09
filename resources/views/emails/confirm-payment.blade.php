08fb01e9ce1a1d44d6f7874a0b08076bc22963fa
ANonymous <postmaster.richmama@gmail.com>
2020-08-09 20:01:54 +0100

    update emails

--- resources/views/emails/confirm-payment.blade.php
+++ resources/views/emails/confirm-payment.blade.php
@@ -1,9 +1,36 @@
 <?php
+  $totals = $order['totals'];
+ $items = $order['items'];
+ $itemCount = $totals['items'];
   $uu = "http://www.aceluxurystore.com/track?o=".$order['reference'];
 ?>
 <center><img src="http://www.aceluxurystore.com/images/logo.png" width="150" height="150"/></center>
 <h3 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">Payment confirmed!</h3>
 Hello {{$name}},<br> your payment for order <b>{{$order['reference']}}</b> has been cleared and your order is being processed. <br><br>
+
+Reference #: <b>{{$order['reference']}}</b><br>
+Customer: <b>{{$name}} ({{$user}})</b><br>
+Notes: <b>{{$order['notes']}}</b><br><br>
+<?php
+foreach($items as $i)
+{
+	$product = $i['product'];
+	$sku = $product['sku'];
+	$qty = $i['qty'];
+	$pu = url('product')."?sku=".$product['sku'];
+	$img = $product['imggs'][0];
+	
+?>
+
+<a href="{{$pu}}" target="_blank">
+  <img style="vertical-align: middle;border:0;line-height: 20px;" src="{{$img}}" alt="{{$sku}}" height="80" width="80" style="margin-bottom: 5px;"/>
+	  {{$sku}}
+</a> (x{{$qty}})<br>
+<?php
+}
+?>
+Total: <b>&#8358;{{number_format($order['amount'],2)}}</b><br><br>
+
 <h5 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">Next steps</h5>
 
 <p>Kindly click the button below to track your delivery. Alternatively you can log in to your Dashboard to track your order (go to Orders and click the Track button beside the order).<br> If you don't have an account you can simply click on <b>Orders</b> on our website and enter your order reference number (<b>{{$order['reference']}})</b></p><br>
