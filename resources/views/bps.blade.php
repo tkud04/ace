f45db7877e0239a961ed8d53f23d4c5f5f264f58
ANonymous <postmaster.richmama@gmail.com>
2020-09-04 20:29:45 +0100

    adding facebook standard events and bps

--- resources/views/bps.blade.php
+++ resources/views/bps.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
-@section('title',"Transaction Successful")
+@section('title',"Order Placed")
 
 @section('content')
 <?php
@@ -8,11 +8,11 @@ $uu = url('confirm-payment')."?oid=".$o['reference'];
 $amount = 0;
 if(isset($o)) $amount = $o['amount'];
 ?>
-<h3 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">Transaction successful</h3>
+<h3 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">Order Placed Successfully</h3>
 
-<b>Your payment was successful!</b><br>
+<b>You've placed your order!</b><br>
 
-<p>Fetching your orders.. <a href="{{$uu}}">Click here</a> if you are not redirected within 10 seconds.</p>
+<p>Confirming your order.. <a href="{{$uu}}">Click here</a> if you are not redirected within 10 seconds.</p>
 
 @stop
 
