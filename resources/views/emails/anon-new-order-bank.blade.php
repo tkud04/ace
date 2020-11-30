64e276eccf8ecc43af7702a091f51463d09d069a
Tobi K <32499735+tkud04@users.noreply.github.com>
2020-11-30 14:53:34 +0100

    Editing emails

--- resources/views/emails/anon-new-order-bank.blade.php
+++ resources/views/emails/anon-new-order-bank.blade.php
@@ -7,9 +7,9 @@
   $cr = $order['courier'];
 ?>
 <center><img src="http://www.aceluxurystore.com/images/logo.png" width="150" height="150"/></center>
-<h3 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">Your order #{{$order['reference']}} is on its way</h3>
+<h3 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">Your order reference # is #{{$order['reference']}}</h3>
 
-Hello {{$u['name']}},<br> You just placed an order on our website. See the details below:<br><br>
+Hello {{$u['name']}},<br> You just placed an order via bank payment. See the details below:<br><br>
 Reference #: <b>{{$order['reference']}}</b><br>
 <?php
 foreach($items as $i)
