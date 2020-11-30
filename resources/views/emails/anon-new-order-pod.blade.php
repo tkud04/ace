456024efd85e1b9580099543252fe39c3e027076
Tobi K <32499735+tkud04@users.noreply.github.com>
2020-11-30 14:59:25 +0100

    editing emails

--- resources/views/emails/anon-new-order-pod.blade.php
+++ resources/views/emails/anon-new-order-pod.blade.php
@@ -7,9 +7,9 @@
   $cr = $order['courier'];
 ?>
 <center><img src="http://www.aceluxurystore.com/images/logo.png" width="150" height="150"/></center>
-<h3 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">Your order #{{$order['reference']}} is on its way</h3>
+<h3 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">Your order reference # is #{{$order['reference']}}</h3>
 
-Hello {{$u['name']}},<br> You just placed an order on our website. See the details below:<br><br>
+Hello {{$u['name']}},<br> You just placed an order via POD (Payment on delivery). See the details below:<br><br>
 Reference #: <b>{{$order['reference']}}</b><br>
 <?php
 foreach($items as $i)
@@ -41,7 +41,7 @@ Total: <b>&#8358;{{number_format($order['amount'],2)}}</b><br><br>
 <h5 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">Next steps</h5>
 <p style="color:red;"><b>NOTE:</b> We only accept <b>CASH</b> on delivery.</p><br>
 <p>Please inspect your order upon arrival to your destination. Once satisfied kindly make your payment to the dispatcher. </p><br>
-<p style="color:red;"><b>NOTE:</b> This order is currently marked as <b>PENDING</b>. We will only process orders whose payment have been cleared in our accounts and confirmed.<br><br>Orders are delivered within 48 hours in Lagos.<br><br>Orders outside Lagos are delivered between 3 – 7 days.</p><br><br>
+<p style="color:red;"><b>NOTE:</b> Orders are delivered within 48 hours in Lagos.<br><br>Orders outside Lagos are delivered between 3 – 7 days.</p><br><br>
 
 <a href="{{$tu}}" target="_blank" style="background: #ff9bbc; color: #fff; padding: 10px 15px; margin-right: 10px;">Track your order</a>
 <br><br>
