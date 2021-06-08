2a2921818cfa80bb11ea88b00caa6c420a00a7d1
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-06-08 21:32:18 +0100

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -382,7 +382,7 @@ class MainController extends Controller {
 		$plugins = $this->helpers->getPlugins();
 		#dd($user);
 		$secure = (isset($req['ss']) && $req['ss'] == "1") ? false : true;
-        $dxf = $req['dxf'];
+        #$dxf = $req['dxf'];
 		if(is_null($user))
 		{
 			return view("anon-checkout",compact(['user','cart','totals','dxf','ss','ad','ref','md','states','secure','c','signals','plugins']));		
