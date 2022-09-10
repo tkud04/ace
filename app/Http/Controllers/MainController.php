c4e681e7aeef2090ca1763541df0512adfe71ce8
Tobi Kudayisi <32499735+tkud04@users.noreply.github.com>
2022-09-10 14:20:02 +0100

    fixing undefined reference bug

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1286,7 +1286,7 @@ class MainController extends Controller {
          {
          	$anon = $this->helpers->getAnonOrder($req['ref']);
 			$orders = [];
-			if(count($anon) > 0)
+			if(count($anon) > 0 && isset($anon['reference']))
 			{
 				$orders[0] = $this->helpers->getOrder($anon['reference'],['reviews' => true]);
 				$banks = $this->helpers->banks;
